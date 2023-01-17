66d080fd66393b15eccfe3a42b7a5d9bb8973a18
ASUS <Yehor Kholodiuk>
2023-01-17 15:51:40 +0200

    initial commit

---
+++ closure.js
@@ -0,0 +1,19 @@
+function changeBalance (){
+    let balance = 0;
+    let key = 'asdf';
+    return function(sum){
+        balance += sum;
+        key = 'adsf';
+        console.log(`Баланс :${balance}`)
+    }
+}
+
+const change = changeBalance();
+change(100);
+change(-50);
+change(200);
+console.dir(change);
+
+const change2 = changeBalance();
+change2(100);
+console.dir(change2)
